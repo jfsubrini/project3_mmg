bc6480089d4d304131a2bb3e7b24a0cdfb8f30c4
jfsubrini <jfsubrini@zoho.com>
2017-09-15 09:07:22 +0200

    Seconde version avec améilioration des constructeurs et instanciations" "

--- mmg_game.py
+++ mmg_game.py
@@ -9,67 +9,67 @@ collect three objects in the maze, and find the way out
 after killing Murdoc, the prison guard.
 
 Python's Scripts
-Files : mmg_game.py, mmg_classes.py, maze_1 and 10 images y 6 sounds
+Files : mmg_game.py, mmg_classes.py, maze_1 and 10 images y 6 sounds.
 
 Copyright Jean-François Subrini, student DA Python at OpenClassrooms.
 """
 
 
-# Importation of the PYGAME Library and locals
+# Importation of the PYGAME Library and locals.
 import pygame
 from pygame.locals import *
 
-# Importation of the other files of the application game
+# Importation of the other files of the application game.
 from mmg_classes import *
 
-# Initialization of the PYGAME Library
+# Initialization of the PYGAME Library.
 pygame.init()
 
-# Game's locals for the size of the Maze
-SPRITE_SIDE = 15		# Number of sprites per side of the Maze        # STAY HERE or in mmg_classes.py ?
-SPRITE_SIZE = 40		# Size of the side of each sprite in pixels     # STAY HERE or in mmg_classes.py ?
-SIDE_SIZE = SPRITE_SIDE * SPRITE_SIZE	# Size of the Maze per side     # STAY HERE or in mmg_classes.py ?
+# Game's locals for the size of the Maze.
+SPRITE_SIDE = 15		# Number of sprites per side of the Maze.       # STAY HERE or in mmg_classes.py ?
+SPRITE_SIZE = 40		# Size of the side of each sprite in pixels.     # STAY HERE or in mmg_classes.py ?
+SIDE_SIZE = SPRITE_SIDE * SPRITE_SIZE	# Size of the Maze per side.    # STAY HERE or in mmg_classes.py ?
 
 # Opening of the screen with the background
 screen = pygame.display.set_mode((SIDE_SIZE, SIDE_SIZE))
-pygame.display.set_caption("MacGyver Maze Game")	           # Title of the game
-FLOOR_IMAGE = pygame.image.load("images/floor.png").convert()  # Loading of the Maze's floor image
+pygame.display.set_caption("MacGyver Maze Game")	           # Title of the game.
+FLOOR_IMAGE = pygame.image.load("images/floor.png").convert()  # Loading of the Maze's floor image.
 
-# Creation of the instances of MacGyver, the Maze and the three objects
-macgyver = Agent()
+# Creation of the instances of MacGyver, the Maze and the three objects.
+macgyver = Agent("images/macgyver.png")
 maze = Maze()
-needle = Objects()
-tube = Objects()
-potion = Objects()
+needle = Object("images/needle.png", maze)
+tube = Object("images/tube.png", maze)
+potion = Object("images/potion.png", maze)
 
 
 # GAME LOOP
 cont = 1
 # INFINITE LOOP
 while cont:
-    # Slowdown the game loop
+    # Slowdown the game loop.
     pygame.time.Clock().tick(40)
     for event in pygame.event.get():
-        # Close the game by closing the window game or typing 'escape'
+        # Close the game by closing the window game or typing 'escape'.
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             cont = 0
         if event.type == KEYDOWN:
             # Keys to move MacGyver
             if event.key == K_RIGHT:
-                macgyver.move('right')
+                macgyver.move('right', maze)
             elif event.key == K_LEFT:
-                macgyver.move('left')
+                macgyver.move('left', maze)
             elif event.key == K_UP:
-                macgyver.move('up')
+                macgyver.move('up', maze)
             elif event.key == K_DOWN:
-                macgyver.move('down')
-    # Blitting of the floor, the Maze and the three objects
+                macgyver.move('down', maze)
+    # Blitting of the floor, the Maze and the three objects.
     screen.blit(FLOOR_IMAGE, (0, 0))
     maze.display_maze(screen)
-    screen.blit(needle.image_n, (0, 160))  # Change the tuple with NEEDLE_POS when random positionning is ready in mmg_classes.py
-    screen.blit(tube.image_t, (560, 120))  # Change the tuple with NEEDLE_POS when random positionning is ready in mmg_classes.py
-    screen.blit(potion.image_p, (280, 400))  # Change the tuple with NEEDLE_POS when random positionning is ready in mmg_classes.py
-    # Blitting of the new position of MacGyver
+    screen.blit(needle.image, (needle.x, needle.y))  # Change the tuple with NEEDLE_POS when random positionning is ready in mmg_classes.py
+    screen.blit(tube.image, (tube.x, tube.y))  # Change the tuple with NEEDLE_POS when random positionning is ready in mmg_classes.py
+    screen.blit(potion.image, (potion.x, potion.y))  # Change the tuple with NEEDLE_POS when random positionning is ready in mmg_classes.py
+    # Blitting of the new position of MacGyver.
     screen.blit(macgyver.image, (macgyver.mg_x, macgyver.mg_y))
-    # Refreshing of the Maze and all the objects
+    # Refreshing of the Maze and all the objects.
     pygame.display.flip()
